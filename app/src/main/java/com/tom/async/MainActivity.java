4c48279d2437cb06c6352fcb19913a59f1481ab7
Hank Tom <hanktom@gmail.com>
2018-01-30 15:22:43 +0800

    Job3Task類別

--- app/src/main/java/com/tom/async/MainActivity.java
+++ app/src/main/java/com/tom/async/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void go3(View view) {
-
+        new Job3Task().execute(6);
     }
 
     class Job1Task extends AsyncTask<Void, Void, Void> {
@@ -50,16 +50,50 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Integer... integers) {
             try {
-                Thread.sleep(integers[0]*1000);
+                Thread.sleep(integers[0] * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             return null;
         }
+
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
             TextView info = findViewById(R.id.info);
+            info.setText("DONE");
+        }
+    }
+
+    class Job3Task extends AsyncTask<Integer, Integer, Void> {
+        private TextView info;
+
+        public Job3Task() {
+            info = findViewById(R.id.info);
+        }
+
+        @Override
+        protected Void doInBackground(Integer... integers) {
+            for (int i = 0; i < integers[0]; i++) {
+                publishProgress(i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            info.setText(String.valueOf(values[0]));
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
             info.setText("DONE");
         }
     }
