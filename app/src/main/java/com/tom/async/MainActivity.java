9f61e330c4c2fe6d91d17c3653458deec757bf97
Hank Tom <hanktom@gmail.com>
2018-01-30 15:05:07 +0800

    使用鏈結建立三個水平分佈的Button元件

--- app/src/main/java/com/tom/async/MainActivity.java
+++ app/src/main/java/com/tom/async/MainActivity.java
@@ -1,7 +1,9 @@
 package com.tom.async;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void go1(View view){
+
+    }
+    public void go2(View view){
+
+    }
+    public void go3(View view){
+
     }
 }
